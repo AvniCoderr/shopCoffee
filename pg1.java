071839c4fc044c375a5a171269583b40dae60ce9
sanjanasans3 <147696074+sanjanasans3@users.noreply.github.com>
2023-10-12 13:04:32 +0530

    Update pg1.java

--- pg1.java
+++ pg1.java
@@ -1,6 +1,7 @@
 public class pg1{
 public static void main(String[]args){
   int i =10;
+  int j=20;
   System.out.println("Data found in roit folder");
   System.out.println("Data found in roit folder");
 
